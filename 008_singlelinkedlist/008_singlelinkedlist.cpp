367a15dc02dea42655ad5e88d695174353f061ee
DzarZaki <dzarzaki2@gmail.com>
2023-05-18 14:34:20 +0700

    Menambahkan blok kode yang akan dijalankan jika kondisi pada if sebelumnya terpenuhi. Pesan 'NIM sudah ada' akan ditampilkan dan fungsi akan langsung diakhiri dengan pernyataan return."

--- 008_singlelinkedlist/008_singlelinkedlist.cpp
+++ 008_singlelinkedlist/008_singlelinkedlist.cpp
@@ -21,4 +21,8 @@ void addNode() {
 	nodeBaru->name = nama;   
 
 	if (START == NULL || nim <= START->noMhs) { 
-		if (START != NULL && nim == START->noMhs)
+		if (START != NULL && nim == START->noMhs) 
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		} 
